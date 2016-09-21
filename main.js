e5b2c5c2e89f16fec4903d2474ff2d81d8a6b3b8
BrodZ11711 <long.brodie@yahoo.com.au>
2016-09-21 20:49:29 +1000

    Added the drawMap function along with variables needed

--- main.js
+++ main.js
@@ -6,24 +6,23 @@ var endFrameMillis = Date.now();
 // This function will return the time in seconds since the function 
 // was last called
 // You should only call this function once per frame
-function getDeltaTime()
-{
-	endFrameMillis = startFrameMillis;
-	startFrameMillis = Date.now();
+function getDeltaTime() {
+    endFrameMillis = startFrameMillis;
+    startFrameMillis = Date.now();
 
-		// Find the delta time (dt) - the change in time since the last drawFrame
-		// We need to modify the delta time to something we can use.
-		// We want 1 to represent 1 second, so if the delta is in milliseconds
-		// we divide it by 1000 (or multiply by 0.001). This will make our 
-		// animations appear at the right speed, though we may need to use
-		// some large values to get objects movement and rotation correct
-	var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
-	
-		// validate that the delta is within range
-	if(deltaTime > 1)
-		deltaTime = 1;
-		
-	return deltaTime;
+    // Find the delta time (dt) - the change in time since the last drawFrame
+    // We need to modify the delta time to something we can use.
+    // We want 1 to represent 1 second, so if the delta is in milliseconds
+    // we divide it by 1000 (or multiply by 0.001). This will make our 
+    // animations appear at the right speed, though we may need to use
+    // some large values to get objects movement and rotation correct
+    var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
+
+    // validate that the delta is within range
+    if (deltaTime > 1)
+        deltaTime = 1;
+
+    return deltaTime;
 }
 
 //-------------------- Don't modify anything above here
@@ -40,41 +39,70 @@ var DEBUG = 1;		// set to 0 to turn off drawing debug information
 var SCREEN_WIDTH = canvas.width;
 var SCREEN_HEIGHT = canvas.height;
 
+//-------------------------------------------BRODIE (Variables for Map)---------------------------------------------------------------
+var LAYER_COUNT = 4; //Layers: Background, Torches, Platforms, Trigger
+var MAP = { tw: 60, th: 20 }; //60 width, 20 height
+var TILE = 35;
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 2;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+//--------------------------------------------------------------------------------------------------------------------------------------
+
 var keyboard = new Keyboard();
 
 var stateManager = new StateManager();
 
-stateManager.pushState( new SplashState() );
+stateManager.pushState(new SplashState());
+//-----------------------------------------BRODIE (Drawing Map)-------------------------------------------------------------------------------------------
+// load the image to use for the level tiles
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+function drawMap() {
+    for (var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++) {
+        var idx = 0;
+        for (var y = 0; y < level1.layers[layerIdx].height; y++) {
+            for (var x = 0; x < level1.layers[layerIdx].width; x++) {
+                if (level1.layers[layerIdx].data[idx] != 0) {
+                    // the tiles in the Tiled map are base 1 (meaning a value of 0 means no tile), so subtract one from the tileset id to get the
+                    // correct tile
+                    var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_X)) * (TILESET_TILE + TILESET_SPACING);
+                    context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x * TILE, (y - 1) * TILE, TILESET_TILE, TILESET_TILE);
+                }
+                idx++;
+            }
+        }
+    }
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+function run() {
+    context.fillStyle = "#ccc";
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
+    var deltaTime = getDeltaTime();
 
-function run()
-{
-	context.fillStyle = "#ccc";		
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	
-	var deltaTime = getDeltaTime();
-	
-	stateManager.update(deltaTime);
-	
-	stateManager.draw();
-			
-	if(DEBUG == 1)
-	{	
-			// update the frame counter 
-		fpsTime += deltaTime;
-		fpsCount++;
-		if(fpsTime >= 1)
-		{
-			fpsTime -= 1;
-			fps = fpsCount;
-			fpsCount = 0;
-		}		
-		
-		// draw the FPS
-		context.fillStyle = "#f00";
-		context.font="14px Arial";
-		context.fillText("FPS: " + fps, 5, 20, 100);
-	}
+    stateManager.update(deltaTime);
+
+    stateManager.draw();
+
+    if (DEBUG == 1) {
+        // update the frame counter 
+        fpsTime += deltaTime;
+        fpsCount++;
+        if (fpsTime >= 1) {
+            fpsTime -= 1;
+            fps = fpsCount;
+            fpsCount = 0;
+        }
+
+        // draw the FPS
+        context.fillStyle = "#f00";
+        context.font = "14px Arial";
+        context.fillText("FPS: " + fps, 5, 20, 100);
+    }
 }
 
 
@@ -83,25 +111,25 @@ function run()
 
 // This code will set up the framework so that the 'run' function is called 60 times per second.
 // We have a some options to fall back on in case the browser doesn't support our preferred method.
-(function() {
-  var onEachFrame;
-  if (window.requestAnimationFrame) {
-    onEachFrame = function(cb) {
-      var _cb = function() { cb(); window.requestAnimationFrame(_cb); }
-      _cb();
-    };
-  } else if (window.mozRequestAnimationFrame) {
-    onEachFrame = function(cb) {
-      var _cb = function() { cb(); window.mozRequestAnimationFrame(_cb); }
-      _cb();
-    };
-  } else {
-    onEachFrame = function(cb) {
-      setInterval(cb, 1000 / 60);
+(function () {
+    var onEachFrame;
+    if (window.requestAnimationFrame) {
+        onEachFrame = function (cb) {
+            var _cb = function () { cb(); window.requestAnimationFrame(_cb); }
+            _cb();
+        };
+    } else if (window.mozRequestAnimationFrame) {
+        onEachFrame = function (cb) {
+            var _cb = function () { cb(); window.mozRequestAnimationFrame(_cb); }
+            _cb();
+        };
+    } else {
+        onEachFrame = function (cb) {
+            setInterval(cb, 1000 / 60);
+        }
     }
-  }
-  
-  window.onEachFrame = onEachFrame;
+
+    window.onEachFrame = onEachFrame;
 })();
 
 window.onEachFrame(run);
